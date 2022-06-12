50be8756905f03fe2758607126839acc39c3c0c1
Cristian Camilo Lopez Niño <cristian.lopez09825@gmail.com>
2022-06-12 11:01:36 -0500

    Se agrego autenticación

--- src/middlewares/oauth/authentication.js
+++ src/middlewares/oauth/authentication.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-
 module.exports.authorize = (authorizedUser) => {
     return async (req, res, next) => {
+        console.log(authorizedUser)
         return next();
     };
 };

60cd38f2e0462b2625feb64d3da51876bf1c245c
Cristian Camilo Lopez Niño <cristian.lopez09825@gmail.com>
2022-06-16 13:57:52 -0500

    Se agrego esquema a los nodos

--- src/models/schemas/node.schema.js
+++ src/models/schemas/node.schema.js
@@ -1,6 +1,11 @@
 const {Schema} = require('mongoose');
 const markerSchema = require('./marker.schema');
 
+const links = new Schema({
+    nodeId: {type: String, required: true}
+});
+
+
 const nodeSchema = new Schema({
     nodeId: {type: String, required: true, unique: true},
     markers: [markerSchema],
@@ -9,7 +14,7 @@ const nodeSchema = new Schema({
     position: {type: String, required: true},
     thumbnail: {type: String, required: true},
     name: {type: String, required: true},
-    links: {type: String, required: true}
+    links: [links]
 });
 
 nodeSchema.plugin(require('mongoose-paginate-v2'));

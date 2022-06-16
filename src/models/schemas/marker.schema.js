1fbc1d6bdf50237dfe5a8b59fcb641064aa56050
Cristian Camilo Lopez Niño <cristian.lopez09825@gmail.com>
2022-06-16 13:36:55 -0500

    Se modifico el esquema correspondiente a la marca

--- src/models/schemas/marker.schema.js
+++ src/models/schemas/marker.schema.js
@@ -1,17 +1,28 @@
 const {Schema} = require('mongoose');
 
-const markerSchema = new Schema({
-    markerId: {type: String, required: true, unique: true},
-    longitude: {type: String, required: true},
-    latitude: {type: String, required: true},
-    image: {type: String, required: true},
-    width: {type: String, required: true},
-    height: {type: String, required: true},
-    anchor: {type: String, required: true},
-    tooltip: {type: String, required: true},
+const tooltip = new Schema({
     content: {type: String, required: true},
-    data: {type: String, required: true}
+    position: {type: String, required: true}
 });
+
+const data = new Schema({
+    generated: {type: Boolean, required: true}
+});
+
+
+const markerSchema = new Schema({
+        markerId: {type: String, required: true, unique: true},
+        longitude: {type: String, required: true},
+        latitude: {type: String, required: true},
+        image: {type: String, required: true},
+        width: {type: String, required: true},
+        height: {type: String, required: true},
+        anchor: {type: String, required: true},
+        tooltip: [tooltip],
+        content: {type: String, required: true},
+        data: [data]
+    }
+);
 
 markerSchema.plugin(require('mongoose-paginate-v2'));
 markerSchema.plugin(require('mongoose-aggregate-paginate-v2'));

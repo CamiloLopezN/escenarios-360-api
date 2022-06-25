76e7fdcfd5e0afcce34ae89adf1c3606609ad4ae
Cristian Camilo Lopez Niño <cristian.lopez09825@gmail.com>
2022-06-25 15:12:40 -0500

    UPDATE

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({extended: false, limit: '50mb'}));
+app.use(express.urlencoded({extended: false, limit: '50mb '}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 

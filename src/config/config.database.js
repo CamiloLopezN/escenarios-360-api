ae7c2e82d295c8b126830f3ef0c0a57cdc1bb8f0
Cristian Camilo Lopez Niño <cristian.lopez09825@gmail.com>
2022-07-01 13:17:01 -0500

    Se agrego algunos cambios xd

--- src/config/config.database.js
+++ src/config/config.database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const {MONGO_USER, MONGO_PASS, MONGO_DB, MONGO_URL} = process.env;
+const {MONGO_USER, MONGO_PASS, MONGO_DB, MONGO_URL, PORT_360_API} = process.env;
 
 /*
 * Opciones de la base de datos
@@ -19,7 +19,7 @@ const mongoOptions = {
 * CONEXION A LA BASE DE DATOS
 * */
 mongoose
-  .connect(MONGO_URL, mongoOptions)
-  .then(() => console.log('Connected to Database!'))
-  .catch((err) => console.log(err));
+    .connect(MONGO_URL, mongoOptions)
+    .then(() => console.log('¡Conexión exitosa con!' + MONGO_DB + ' en el puerto: ' + PORT_360_API))
+    .catch((err) => console.log(err));
 module.exports = mongoose;
